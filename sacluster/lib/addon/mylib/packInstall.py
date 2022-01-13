c7dc39fd0ab8375c858be534b148bbb3fe068e59
森保歩美 <>
2022-01-13 09:51:14 +0900

    パッケージインストール(packInstall)の更新

--- sacluster/lib/addon/mylib/packInstall.py
+++ sacluster/lib/addon/mylib/packInstall.py
@@ -2,6 +2,7 @@
 import sys
 import time
 import logging
+from paramiko import channel
 import tqdm
 import numpy as np
 import os
@@ -18,48 +19,66 @@ import asyncio
 import paramiko 
 
 def packInstall():
-    #ヘッドノード
-    IP_ADDRESS = '133.242.50.16'
-    PORT = 22
-    USER = 'root'
-    PASSWORD = 'test001pw'
+    #管理ノード
+    IP_ADDRESS1 = '133.242.50.16'
+    PORT1 = 22
+    USER1 = 'root'
+    PASSWORD1 = 'test01pw'
+    KYE='~/.ssh/id_rsa.pub'
 
 
-    #ホストサーバに接続
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.WarningPolicy())
+    #管理ノードに接続
+    headnode = paramiko.SSHClient()
+    headnode.set_missing_host_key_policy(paramiko.WarningPolicy())
 
-    #接続
-    client.connect(IP_ADDRESS, PORT, USER, PASSWORD)
+    print("hostnode connecting...")
+    headnode.connect(hostname=IP_ADDRESS1, port=PORT1, username=USER1, password=PASSWORD1)
+    print('hostnode connected')
 
-    #cmdの実行ここでインストール
-    stdin, stdout, stderr = client.exec_command('yum -y install nfs-utils')
-    #次の処理まで待機(一秒待つ)
-    #time.sleep(5)
-    #stdin, stdout, stderr = client.exec_command('yum -y remove nfs-utils')
-    #time.sleep(5)
-    #stdin, stdout, stderr = client.exec_command('yum list')
+#コマンド実行
+    stdin, stdout, stderr = headnode.exec_command('hostname')
+    time.sleep(1)
+    hostname = stdout.read().decode()
+    print('hostname_head = %s' % hostname)
 
-    print(stdout)
+    #管理->計算ノード
+    #計算ノード
+    IP_ADDRESS2 = '192.168.100.1'
+    PORT2 = 22
+    USER2 = 'root'
+    PASSWORD2 = 'test01pw'
+
+
+    head = (IP_ADDRESS1,PORT1)
+    compute = (IP_ADDRESS2, PORT2)
+    transport1 = headnode.get_transport()
+    channel1 = transport1.open_channel("direct-tcpip", compute, head)
+    computenode = paramiko.SSHClient()
+    computenode.set_missing_host_key_policy(paramiko.WarningPolicy())
+
+    print("compurenode connecting...")
+    computenode.connect(hostname=IP_ADDRESS2,username=USER2,password=PASSWORD2,sock=channel1)
+    print('computenode connected')
+
+#コマンド実行
+    (stdin, stdout, stderr) = computenode.exec_command('yum -y install ypbind')
+    time.sleep(1)
+    hostname = stdout.read().decode()
+    print('hostname_compute = %s' % hostname)
 
     # コマンド実行結果を変数に格納
-    cmd_result = ''
-    for line in stdout:
-        cmd_result += line
+    #cmd_result = ''
+    #for line in stdout:
+     #   cmd_result += line
 
     # 実行結果を出力
-    print(cmd_result)
+    #print(cmd_result)
 
     #コネクションを閉じる
-    client.close()
-    del client, stdin, stdout, stderr
+    headnode.close()
+    computenode.close()
+    del headnode, computenode, stdin, stdout, stderr
 
 if __name__ == '__main__':
     packInstall ()
 
-#プログレスバー
-import time
-from tqdm import tqdm
- 
-for i in tqdm(range(100)):
-    time.sleep(0.01)
