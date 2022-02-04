742222c2a24a1f67033b5d2e89635d7e971ca060
LAPTOP-R70JU6F6\KosukeTSUJI <q232057k@mail.kyutech.jp>
2022-02-05 01:28:36 +0900

    指定したポートの開放を行う
    引数には複数のポート番号とそのポートのprotocolを指定する必要があります
    設定を行っているゾーンは Trusted なのでpubricの設定を行いたい場合はこの関数は使えないです．
    
    HEAD，COMP，ALLで操作対象を指定することができます

--- sacluster/lib/addon/addon_main.py
+++ sacluster/lib/addon/addon_main.py
@@ -21,9 +21,10 @@ from info_print import printout
 sys.path.append(common_path + "/lib/addon/mylib")
 from editHost import editHost
 from getClusterInfo import getClusterInfo
+from portOpen import portOpen
 sys.path.append(common_path + "/lib/addon/setupIP")
 from setupIpEth1 import setupIpEth1
-import switchFWZone
+from switchFWZone import switchFWZone
 sys.path.append(common_path + "/lib/addon/setupProxy")
 from proxySetup import proxySetup
 
@@ -33,9 +34,13 @@ def addon_main(clusterID):
     print("アドオンの関数")
     params = getClusterInfo()                               # メモ：クラスター情報を取得して変数に入れました
     setupIpEth1(clusterID, params, nodePassword='test')     # メモ：コンピュートノードに対する操作確認できないから誰か確認お願いします
-    editHost()
-    swhichFWZone()                                          # <-- 森保さん，これ関数名変じゃないですか？インポート文も修正しておいてください
-    # portOpen()
+    # editHost()
+    swhichFWZone()
+    
+    portOpen(clusterID, params, nodePassword='test',    
+            changeTarget = "HEAD",                          # "HEAD", "COMP", "ALL" のいずれかを選択
+            ports        = [4000, 4001], 
+            protocols    = ['udp', 'tcp'])
     proxySetup(clusterID, params, nodePassword='test')      # メモ：ファイアウォールの設定してないとコンピュートノードから外部に通信できないので注意
     # monitorSetup()
 
