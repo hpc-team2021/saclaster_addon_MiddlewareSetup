5f3da1408b0da2a914c5251bbdab4b0e07b62507
Daiki_M <takechin0905@gmail.com>
2022-02-07 20:51:18 +0900

    jsonファイル読み込み用関数の追加

--- sacluster/lib/addon/addon_main.py
+++ sacluster/lib/addon/addon_main.py
@@ -21,6 +21,7 @@ from info_print import printout
 sys.path.append(common_path + "/lib/addon/mylib")
 from editHost import editHost
 from getClusterInfo import getClusterInfo
+from loadAddonParams import loadAddonParams
 sys.path.append(common_path + "/lib/addon/setupIP")
 from setupIpEth1 import setupIpEth1
 import switchFWZone
@@ -31,17 +32,19 @@ from proxySetup import proxySetup
 def addon_main(clusterID):
 
     print("アドオンの関数")
-    params = getClusterInfo()                               # メモ：クラスター情報を取得して変数に入れました
-    setupIpEth1(clusterID, params, nodePassword='test')     # メモ：コンピュートノードに対する操作確認できないから誰か確認お願いします
-    editHost()
-    swhichFWZone()                                          # <-- 森保さん，これ関数名変じゃないですか？インポート文も修正しておいてください
-    # portOpen()
-    proxySetup(clusterID, params, nodePassword='test')      # メモ：ファイアウォールの設定してないとコンピュートノードから外部に通信できないので注意
-    # monitorSetup()
+    params = getClusterInfo ()                                              # メモ：クラスター情報を取得して変数に入れました
+    jsonAddonParams = loadAddonParams ()
+    setupIpEth1 (clusterID, params, nodePassword = 'test')                  # メモ：コンピュートノードに対する操作確認できないから誰か確認お願いします
+    editHost (clusterID, params, jsonAddonParams, nodePassword = 'test')
+    # swhichFWZone ()                                                       # <-- 森保さん，これ関数名変じゃないですか？インポート文も修正しておいてください
+    # portOpen (jsonAddonParams)
+    proxySetup (clusterID, params, nodePassword = 'test')                   # メモ：ファイアウォールの設定してないとコンピュートノードから外部に通信できないので注意
+    # monitorSetup ()
 
-def addon_start():
-    print("ミドルウェアの起動")
+def addon_start ():
+    print ("ミドルウェアの起動")
+    # daemonStart ()
 
 if __name__ == '__main__':
-    clusterID = "576968"
-    sys.exit(addon_main(clusterID))
+    clusterID = "983867"
+    sys.exit ( addon_main (clusterID) )
---
+++ sacluster/lib/addon/mylib/loadAddonParams.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+# User defined Library
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+common_path = os.path.abspath("../../..")
+
+fileName = common_path + "\\lib\\addon\\setting.json"
+
+def loadAddonParams ():
+    # Load Command data
+    json_open = open(fileName, 'r')
+    jsonAddonParams = json.load(json_open)
+    
+    return jsonAddonParams
