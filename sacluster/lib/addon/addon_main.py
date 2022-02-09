aac917da82f28e43da92099174f78c5434ffb716
LAPTOP-R70JU6F6\KosukeTSUJI <q232057k@mail.kyutech.jp>
2022-02-10 03:38:15 +0900

    引数の統一とインデントをそろえました．
    swhichFWZone()の引数とか決まってたら統一していただけると助かります
    monitorSetup ()のためにportOpen()の引数設定しているがjsonファイルの内容は確認していないのでmonitorSetup ()編集する人はそちらの確認もお願いします

--- sacluster/lib/addon/addon_main.py
+++ sacluster/lib/addon/addon_main.py
@@ -19,26 +19,27 @@ from API_method import get, post, put, delete
 from info_print import printout
 
 sys.path.append(common_path + "/lib/addon/mylib")
-from editHost import editHost
-from getClusterInfo import getClusterInfo
-from loadAddonParams import loadAddonParams
+from editHost           import editHost
+from getClusterInfo     import getClusterInfo
+from loadAddonParams    import loadAddonParams
+from portOpen           import portOpen
 sys.path.append(common_path + "/lib/addon/setupIP")
-from setupIpEth1 import setupIpEth1
-import switchFWZone
+from setupIpEth1        import setupIpEth1
+from switchFWZone       import switchFWZone
 sys.path.append(common_path + "/lib/addon/setupProxy")
-from proxySetup import proxySetup
+from proxySetup         import proxySetup
 
 
 def addon_main(clusterID):
-
-    print("アドオンの関数")
-    params = getClusterInfo ()                                              # メモ：クラスター情報を取得して変数に入れました
+    params = getClusterInfo ()
     jsonAddonParams = loadAddonParams ()
-    setupIpEth1 (clusterID, params, nodePassword = 'test')                  # メモ：コンピュートノードに対する操作確認できないから誰か確認お願いします
-    editHost (clusterID, params, jsonAddonParams, nodePassword = 'test')
-    # swhichFWZone ()                                                       # <-- 森保さん，これ関数名変じゃないですか？インポート文も修正しておいてください
-    # portOpen (jsonAddonParams)
-    proxySetup (clusterID, params, nodePassword = 'test')                   # メモ：ファイアウォールの設定してないとコンピュートノードから外部に通信できないので注意
+
+    setupIpEth1 (clusterID, params, nodePassword = 'test')
+    editHost    (clusterID, params, nodePassword = 'test', jsonAddonParams = jsonAddonParams)
+    swhichFWZone()
+    portOpen    (clusterID, params, nodePassword = 'test', jsonAddonParams = jsonAddonParams, serviceType="Proxy"  , serviceName="Squid")
+    proxySetup  (clusterID, params, nodePassword = 'test', jsonAddonParams = jsonAddonParams)
+    # portOpen    (clusterID, params, nodePassword = 'test', jsonAddonParams = jsonAddonParams, serviceType="Monitor", serviceName="Ganglia")
     # monitorSetup ()
 
 def addon_start ():
@@ -46,5 +47,5 @@ def addon_start ():
     # daemonStart ()
 
 if __name__ == '__main__':
-    clusterID = "983867"
-    sys.exit ( addon_main (clusterID) )
+    clusterID = "927941"
+    sys.exit (addon_main (clusterID))
