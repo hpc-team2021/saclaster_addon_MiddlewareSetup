b3e4e08d1c5e6409f9a477cc2ba2d84f2b64a3f3
KosukeTSUJI <q232057k@mail.kyutech.jp>
2022-08-08 09:43:16 +0900

    debug
    削除されたファイルの読み込み処理部分を消しました

--- sacluster/lib/command/command_pro.py
+++ sacluster/lib/command/command_pro.py
@@ -35,11 +35,6 @@ from ps_main import ps_main
 sys.path.append(common_path + "/lib/addon")
 from addon_main import addon_main
 
-sys.path.append(common_path + "/lib/addon/setupIP")
-from setup_ip_eth0 import setup_ip_eth0
-
-
-
 import logging
 
 dt_now = datetime.datetime.now()
@@ -379,4 +374,4 @@ def command_main():
         args.handler(args)
 
 if __name__ == "__main__":
-    command_main()
+   
